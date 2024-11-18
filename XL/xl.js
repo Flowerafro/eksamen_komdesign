be72488f88b7dea9b919914330f3d0583e6b1679
Flowerafro <linehenriksen0@gmail.com>
2024-11-18 18:56:01 +0100

    Sentralisering dreper main title

--- XL/xl.js
+++ XL/xl.js
@@ -1,4 +1,29 @@
+/* pulsteller */
 
+let counter = 60;
+const maxCount = 240;
+
+/* const intervalId = setInterval(() => {
+    document.getElementById("counter").innerText = counter;
+    console.log(counter);
+    counter++;
+
+    if (counter > maxCount) {
+        clearInterval(intervalId);
+    }
+}, 1000); */
+
+document.addEventListener('scroll', function () {
+    document.querySelectorAll('.article').forEach((article) => {
+        if (isInViewport(article)) {
+            console.log(`Article ${article.id} is in viewport`);
+            counter++;
+        }
+    })
+
+})
+
+/* Bytte bilder til første kartet */
 function isInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
@@ -9,15 +34,6 @@ function isInViewport(el) {
             rect.right <= (window.innerWidth || document.documentElement.clientWidth))
     );
 }
-
-/* document.addEventListener('DOMContentLoaded', function () {
-    if (isInViewport(document.getElementById("intro"))) {
-        document.getElementById("parallax-1-bg").style.backgroundImage = "IMG/tofte1.png";
-    }
-}, {
-    passive: true
-});
- */
 
 document.addEventListener('scroll', function () {
     const elements = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth"];
@@ -35,3 +51,28 @@ document.addEventListener('scroll', function () {
 });
 
 
+/* Bytte av bakgrunnsfarge på hovedtittel */
+function isPartiallyInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top < window.innerHeight && rect.bottom >= 0
+    );
+}
+
+document.addEventListener('scroll', function () {
+    const intro = document.getElementById("intro");
+    const mainTitle = document.getElementById("main-title");
+    const buffer = document.getElementById("buffer");
+
+    if (isPartiallyInViewport(mainTitle)) {
+        console.log(`Element mainTitle is partially in viewport`);
+        intro.classList.add("red-background");
+        mainTitle.classList.add("red-background");
+        buffer.classList.add("red-background");
+    } else {
+        intro.classList.remove("red-background");
+        mainTitle.classList.remove("red-background");
+        buffer.classList.remove("red-background");
+    }
+});
+/* const lastPulse = document.getElementById("lastpulse"); */
